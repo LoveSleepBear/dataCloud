b50a1b41dd765159d26958b1d56f4de13109124a
LoveSleepBear <lovesleepbear@163.com>
2018-06-21 22:00:18 +0800

    添加删除controller

--- src/main/java/com/syb/datacloud/controller/DeleteFileController.java
+++ src/main/java/com/syb/datacloud/controller/DeleteFileController.java
@@ -1,5 +1,11 @@
 package com.syb.datacloud.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.syb.datacloud.service.FileService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("delete")
 public class DeleteFileController {
+
+    @Autowired
+    private FileService fileService;
+
+    /**
+     *
+     * @param strData
+     * @return
+     */
+    public String deleteFile(@RequestBody String strData){
+        if (StringUtils.isBlank(strData)){
+            return "请输入参数";
+        }
+
+        JSONObject jsonObject = JSON.parseObject(strData);
+        String fileId = jsonObject.getString("fileId");
+        String fileName = jsonObject.getString("fileName");
+
+
+
+        return strData;
+    }
+
 }
